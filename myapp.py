e1f9e59cf32869631c4b24eae74afd3f8ee37e47
Sarfaraz Ahmed Abbasi <SarfarazAAbbasi+GitHub@Gmail.Com>
2022-02-12 22:37:02 +0500

    My First Streamlit App From Tutorial

---
+++ myapp.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of Google!
+""")
+
+# Define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical prices for this ticker
+tickerDf = tickerData.history(period = '1d', start = '2012-02-12', end = '2022-02-12')
+# Open  High    Low Close   Volume  Dividends   Stock   Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
